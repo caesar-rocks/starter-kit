799b673cd3a812892898f0e9ea6fcecce294d8af
Alexis <80361585+alexisbouchez@users.noreply.github.com>
2024-06-19 12:17:29 +0200

    make the starter kit ready for CaesarUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
   content: ["./views/**/*.templ"],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
 };
